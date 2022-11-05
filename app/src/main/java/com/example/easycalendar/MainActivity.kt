2fad0ea9198bd3c7796acfff532c3c678a3d4776
hamcoding9 <dientes8520@gmail.com>
2022-11-05 18:24:27 +0900

    [feat] Naver Login 기능 추가

--- app/src/main/java/com/example/easycalendar/MainActivity.kt
+++ app/src/main/java/com/example/easycalendar/MainActivity.kt
@@ -1,11 +1,66 @@
 package com.example.easycalendar
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity.RESULT_CANCELED
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.example.easycalendar.databinding.ActivityMainBinding
+import com.navercorp.nid.NaverIdLoginSDK
+import com.navercorp.nid.NaverIdLoginSDK.getAccessToken
+import com.navercorp.nid.oauth.NidOAuthPreferencesManager.accessToken
+import com.navercorp.nid.oauth.NidOAuthPreferencesManager.errorCode
+import com.navercorp.nid.oauth.NidOAuthPreferencesManager.errorDescription
+import com.navercorp.nid.oauth.NidOAuthPreferencesManager.refreshToken
+import com.navercorp.nid.oauth.OAuthLoginCallback
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        NaverIdLoginSDK.initialize(
+            applicationContext,
+            getString(R.string.naver_client_id),
+            getString(R.string.naver_client_secret),
+            getString(R.string.naver_client_name)
+        )
+        binding.buttonOAuthLoginImg.setOAuthLogin(launcher, oauthLoginCallback)
+    }
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        when(result.resultCode) {
+            RESULT_OK -> {
+                // 네이버 로그인 인증이 성공했을 때 수행할 코드 추가
+                Toast.makeText(applicationContext, "로그인 성공", Toast.LENGTH_SHORT).show()
+            }
+            RESULT_CANCELED -> {
+                // 실패 or 에러
+                val errorCode = NaverIdLoginSDK.getLastErrorCode().code
+                val errorDescription = NaverIdLoginSDK.getLastErrorDescription()
+                Toast.makeText(applicationContext, "errorCode:$errorCode, errorDesc:$errorDescription", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private val oauthLoginCallback = object : OAuthLoginCallback {
+        override fun onSuccess() {
+            // 네이버 로그인 인증이 성공했을 때 수행할 코드 추가
+            Toast.makeText(applicationContext, "로그인 성공", Toast.LENGTH_SHORT).show()
+        }
+        override fun onFailure(httpStatus: Int, message: String) {
+            val errorCode = NaverIdLoginSDK.getLastErrorCode().code
+            val errorDescription = NaverIdLoginSDK.getLastErrorDescription()
+            Toast.makeText(applicationContext,"errorCode:$errorCode, errorDesc:$errorDescription",Toast.LENGTH_SHORT).show()
+        }
+        override fun onError(errorCode: Int, message: String) {
+            onFailure(errorCode, message)
+        }
     }
 }
